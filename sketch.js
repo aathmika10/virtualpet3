b1960bc7bd15e947bfc0d04a89b27e5e8658c721
aathmika10 <65343462+aathmika10@users.noreply.github.com>
2020-08-08 14:38:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,17 +2,20 @@
 var dog, happyDog;
 var database;
 var foodS, foodStock;
-var fedTime, lastFed;
+var fedTime;
 var feed, addFoodCount;
 var foodObj;
-var changeGameState, readGameState;
 var bedroomimg, washroomimg, gardenimg;
-var lazydogimg
+var lazydogimg;
 var hungry=0;
 var Sleeping=0;
 var Playing=0;
 var Bathing=0;
 var gameState=0;
+var currentTime;
+var lastFed=hour;
+var currentTime=hour;
+var hr=hour;
 
 function preload()
 {
@@ -24,12 +27,12 @@ function preload()
   gardenimg = loadImage("Garden.png")
   lazydogimg = loadImage("Lazy.png")
   dogkennelimg=loadImage("dogkennel.jpg");
-  //milkBottleimage = loadImage("Milk.png")
 }
 
 function setup() {
   createCanvas(370, 550);
 
+  //refering the database
   database=firebase.database();
     foodStock=database.ref('foodStock');
     foodStock.on("value",readStock);
@@ -39,47 +42,57 @@ function setup() {
       gameState=data.val();
     })
 
+  //Creating the foodObj
   foodObj= new Food();
 
+  //creating the dog sprite
   dog=createSprite(200, 435, 10,10);
   dog.addImage(dogimg);
   dog.scale=0.2;
-
+  
+  //creating the buttons
+    //FEED BUTTON
   feed=createButton("Feed the dog");
   feed.position(80,570);
   feed.mousePressed(feedDog);
-
+    //ADD FOOD
   addFoodCount=createButton("Add Food")
   addFoodCount.position(300,570);
   addFoodCount.mousePressed(addFood);
-
- 
 }
 
 function draw() {  
   background(dogkennelimg);
 
-
- 
+  //displaying the food object
    foodObj.display();
-    
-  if (gameState!=hungry){
+
+    //Destroying the buttons and making the dog lazy
+   /*if(gameState!=hungry){
     feed.hide();
     addFoodCount.hide();
     dog.addImage(lazydogimg);
     dog.y=460
+   }*/
+
+  if (gameState=hungry){
+     feed.hide();
+    addFoodCount.hide();
+    dog.addImage(lazydogimg);
+    dog.y=460
   }
-  else{
+ else{
     feed.show();
     addFoodCount.show();
     dog.addImage(lazydogimg)
   }
 
-  currentTime=hour();
+//changing the gameState, background according to the time
+  currentTime= hour();
 if(currentTime===(lastFed+1)){
+  gameState==Playing
   update("Playing");
   foodObj.Garden();
- 
 }
 else if(currentTime==(lastFed+2)){
   update("Sleeping")
@@ -94,29 +107,33 @@ else{
   foodObj.display();
 }
 
+//Displaying the text
     textSize(16);
     textFont("Comic sans MS");
     fill("white");
-    text("Last Fed:"+currentTime,150,545);
     text("Food remaining:"+foodS,120,15);
-
-   drawSprites();  
+    text("Current hour:" + lastFed, 5, 50);
+    console.log(hour);
+    hr = hour();
+   // text("Current hour:" + hr, 5, 50);
+   // text("Last Fed:"+lastFed,150,545);
+    
+  drawSprites();  
 }
-
 
 //funtion to updatefood stock and last fedtime
   function feedDog(){
     dog.addImage(happyDogimg);
     foodObj.updateFoodStock(foodS);
     database.ref('/').update({
-      //Food:foodObj.getFoodStock(),
-      FeedTime:hour()
+    Food:foodObj.getFoodStock(),
+    FeedTime: hour()
     })
   }
+  
   //function to add food in stock
-function addFood(){
+  function addFood(){
   foodS++;
-  //console.log(foodS)
   database.ref('/').update({
   foodStock:foodS
   })
@@ -124,7 +141,7 @@ function addFood(){
 
 function readStock(data){
   foodS=data.val();
- // foodObj.updateFoodStock(foodS);
+  foodObj.updateFoodStock(foodS);
 }
 
 function update(state){
